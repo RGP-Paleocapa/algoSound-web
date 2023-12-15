7ce6a0a8146208c9547ae52b61fe978f43a40b13
Puiu Rares Gabriel <puiu.raresgabriel.studente@itispaleocapa.it>
2023-12-15 11:30:29 +0100

    .

--- src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './home/Home';
-export { default as Coding } from './coding/Coding';
-export { default as Explanation } from './explanation/Explanation';
-export { default as Admin } from './admin/Admin';
-export { default as ProtectedPage } from './protectedPage/ProtectedPage';
--- src/pages/protectedPage/ProtectedPage.tsx
+++
@@ -1,9 +0,0 @@
-const ProtectedPage = () => {
-    return (
-        <>
-            <h1 className="my-4 dark:text-white text-6xl">Protected Page</h1>
-        </>
-    );
-}
-
-export default ProtectedPage;
